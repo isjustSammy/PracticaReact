228400a87f63f3cf79ca947fcabc8850b5ea90ce
Oscar Rauda <20220631@ricaldone.edu.sv>
2025-03-20 09:29:40 -0600

    Practica

---
+++ practica-rauda/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import Boton from './components/Boton'
+function App() {
+
+
+  return (
+    <>
+    <h1>Hola Mundo</h1>
+    <Boton>titulo={'Boton 1'}</Boton>
+    <Boton>titulo={'Boton 2'}</Boton>
+    <Boton>titulo={'Boton 3'}</Boton>
+    <Boton>titulo={'Boton 4'}</Boton>
+    <Boton>titulo={'Boton 5'}</Boton>
+    <Boton>titulo={'Boton 6'}</Boton>
+    
+    </>
+  )
+}
+
+export default App
---
+++ practica-rauda/src/components/Boton.jsx
@@ -0,0 +1,19 @@
+import './boton.css';
+const Boton =({titulo})=>{
+
+
+
+const estilo={
+    Boton:{
+
+        
+    }
+}
+
+    return(
+        <button type="button" className='boton'>Dark {titulo} </button>
+        
+    )
+}
+
+export default Boton;
